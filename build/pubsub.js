c5bde03d441a7d6a9f1225edd6a12aaff951d5f3
Bruce Lane <blane@ALBERTVIEILLE.local>
2018-01-09 14:32:07 +0100

    scripts added in build

---
+++ build/pubsub.js
@@ -0,0 +1,34 @@
+console.log('toto');
+/*var PubSubManager = {
+  subscribers: [],
+
+  subscribe: function(parent, callback) {
+    this.subscribers.push({ parent: parent, callback: callback });
+  },
+
+  color: function(name) {
+    // Notify subscribers of event.
+    this.subscribers.forEach(function(subscriber) {
+      subscriber.callback(name, subscriber.parent);
+    });
+  }
+};
+*/
+$(function() {
+   setTimeout(function() {
+    console.log('timeout')
+    ReactDOM.render(React.createElement(Periode, { context: PubSubManager, body: $('body')}), document.getElementById('root'));
+  }, 0);
+
+  // Change the box color every few seconds.
+  setInterval(function() {
+    var box = $('#box');
+    var color = box.css('background-color') === 'rgb(255, 0, 0)' ? 'orange' : 'red';
+
+    // Change box color.
+    box.css('background-color', color);
+
+    // Notify subscribers.
+    PubSubManager.color(color);
+  }, 3000)
+});
